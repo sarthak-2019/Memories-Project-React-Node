0fe6790db47385153714fab19a59ac6860d058be
Sarthak Shukla <sarthak2019fractal@gmail.com>
2021-07-13 23:26:27 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import postRoutes from './routes/posts.js';
+const app = express();
+
+app.use(express.json({ limit: '30mb', extended: true }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+const CONNECTION_URL =
+  'mongodb+srv://Sarthak:D8mGDa2wT7IqoLTt@cluster0.w2o8g.mongodb.net/Memories?retryWrites=true&w=majority';
+const PORT = process.env.port || 8000;
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
